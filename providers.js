57970edfb9dcabc5ad1562e376fd262bed240b2e
MOVscan <paymoneyy068@gmail.com>
2026-01-31 12:34:41 +0700

    Create providers.js

---
+++ providers.js
@@ -0,0 +1,17 @@
+export const providers = [
+  {
+    name: "CloudAlpha",
+    region: "US",
+    pricePerGBHour: 0.0042
+  },
+  {
+    name: "CloudBeta",
+    region: "EU",
+    pricePerGBHour: 0.0038
+  },
+  {
+    name: "CloudGamma",
+    region: "Asia",
+    pricePerGBHour: 0.0031
+  }
+];
